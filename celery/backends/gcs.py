0246129818308e557024cb378cbfee7a12103759
Haim Daniel <haim@jether-energy.com>
2024-02-15 19:37:34 +0200

    wip: Add GCS backend start

---
+++ celery/backends/gcs.py
@@ -0,0 +1,113 @@
+"""GCS result store backend."""
+import os
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
+from threading import RLock
+
+import requests
+
+from celery.exceptions import ImproperlyConfigured
+from kombu.utils import cached_property
+from kombu.utils.encoding import bytes_to_str
+from .base import KeyValueStoreBackend
+
+try:
+    from google.cloud import storage
+    from google.api_core.retry import Retry
+except ImportError:
+    storage = None
+
+from urllib.parse import urlparse
+
+
+__all__ = ('GCSBackend',)
+
+
+class GCSBackend(KeyValueStoreBackend):
+    """Google Cloud Storage task result backend."""
+
+    _lock = RLock()
+    _my_pid = os.getpid()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        if not storage:
+            raise ImproperlyConfigured(
+                'You must install google-cloud-storage to use gcs backend'
+            )
+        self._init_from_url()
+        self._client = None
+
+    def _init_from_url(self):
+        res = urlparse(self.url)
+        if res.scheme != 'gs':
+            raise ImproperlyConfigured(
+                f'Invalid result_backend URL: {self.url}'
+            )
+        self.bucket_name = res.netloc
+        self.base_path = res.path.strip('/')
+        params = dict(kv.split('=') for kv in res.query.split('?'))
+        if 'project' not in params:
+            raise ImproperlyConfigured(
+                'You must specify a project in the result_backend URI'
+            )
+        self.project = params['project']
+        self.ttl = int(params.get('ttl', 0))
+
+    def get(self, key):
+        key = bytes_to_str(key)
+        blob = self._get_blob(key)
+        try:
+            return blob.download_as_bytes()
+        except storage.blob.NotFound:
+            return None
+
+    def set(self, key, value):
+        key = bytes_to_str(key)
+        blob = self._get_blob(key)
+        if self.ttl:
+            blob.custom_time = datetime.utcnow() + timedelta(seconds=self.ttl)
+        Retry()(blob.upload_from_string)(value)
+
+    def delete(self, key):
+        key = bytes_to_str(key)
+        blob = self._get_blob(key)
+        if blob.exists():
+            Retry()(blob.delete)()
+
+    def mget(self, keys):
+        with ThreadPoolExecutor() as pool:
+            return list(pool.map(self.get, keys))
+
+    @property
+    def client(self):
+        """Returns a storage client."""
+
+        if self._client:
+            return self._client
+
+        # make sure it's thread-safe, as creating a new client is expensive
+        with self._lock:
+            if self._client and self._my_pid == os.getpid():
+                return self._client
+            # Make sure each process gets its own connection after a fork
+            self._client = storage.Client(project=self.project)
+            self._my_pid = os.getpid()
+
+            # Increase the number of connections to the server
+            adapter = requests.adapters.HTTPAdapter(
+                pool_connections=128, pool_maxsize=128, max_retries=3
+            )
+            self._client._http.mount("https://", adapter)
+            self._client._http._auth_request.session.mount("https://", adapter)
+
+            return self._client
+
+    @cached_property
+    def bucket(self):
+        return storage.bucket.Bucket(client=self.client, name=self.bucket_name)
+
+    def _get_blob(self, key):
+        key_bucket_path = f'{self.base_path}/{key}' if self.base_path else key
+        return self.bucket.blob(key_bucket_path)
